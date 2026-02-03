fa4f09cda6fc63f4b15dbf865483e5dc377fd7af
Augusto <augustovanderley@gmail.com>
2026-02-03 00:24:34 -0300

    fix: correct weight data (issue #3) - Base Frente R->L

--- js/data.js
+++ js/data.js
@@ -6,80 +6,80 @@ window.ForroDB = [
         "id": "basic_step_roots_frente",
         "name": "Base para frente (Roots)",
         "type": "Roots",
-        "start_foot": "L",
-        "end_foot": "L",
+        "initial_weight": "R",
+        "final_weight": "L",
         "beats": 4
     },
     {
         "id": "basic_step_roots_tras",
         "name": "Base para trás (Roots)",
         "type": "Roots",
-        "start_foot": "R",
-        "end_foot": "R",
+        "initial_weight": "L",
+        "final_weight": "R",
         "beats": 4
     },
     {
         "id": "basic_step_univ",
         "name": "Base (Universitário)",
         "type": "Universitário",
-        "start_foot": "L",
-        "end_foot": "R",
+        "initial_weight": "R",
+        "final_weight": "L",
         "beats": 4
     },
     {
         "id": "abertura",
         "name": "Abertura",
         "type": "Universitário",
-        "start_foot": "L",
-        "end_foot": "R",
+        "initial_weight": "R",
+        "final_weight": "L",
         "beats": 4
     },
     {
         "id": "giro_simples",
         "name": "Giro Simples",
         "type": "Universitário",
-        "start_foot": "L",
-        "end_foot": "L",
+        "initial_weight": "R",
+        "final_weight": "R",
         "beats": 4
     },
     {
         "id": "caminhada",
         "name": "Caminhada",
         "type": "Roots",
-        "start_foot": "L",
-        "end_foot": "L",
+        "initial_weight": "R",
+        "final_weight": "L",
         "beats": 4
     },
     {
         "id": "sacada",
         "name": "Sacada",
         "type": "Roots",
-        "start_foot": "L",
-        "end_foot": "R",
+        "initial_weight": "R",
+        "final_weight": "L",
         "beats": 4
     },
     {
         "id": "trava",
         "name": "Trava",
         "type": "Roots",
-        "start_foot": "L",
-        "end_foot": "L",
+        "initial_weight": "R",
+        "final_weight": "L",
         "beats": 2
     },
     {
         "id": "leque",
         "name": "Leque",
         "type": "Roots",
-        "start_foot": "L",
-        "end_foot": "R",
+        "initial_weight": "R",
+        "final_weight": "L",
         "beats": 8
     },
     {
         "id": "facao",
         "name": "Facão",
         "type": ["Roots", "Universitário"],
-        "start_foot": "R",
-        "end_foot": "R",
+        "initial_weight": "L",
+        "final_weight": "R",
         "beats": 4
     }
 ];
